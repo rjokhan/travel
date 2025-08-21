a23a4d72762e0d1497661bfba73e8a7b7dcf1917
rjokhan <jaidonswift@gmail.com>
2025-08-21 23:05:01 +0500

    update

--- travelweb/travelweb/urls.py
+++ travelweb/travelweb/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.urls import path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),               # <- стандартная админка
@@ -18,4 +21,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("accounts.urls")),
     path("", include("travelapp.urls")),
+    path("df261d9c024af55022f04b95a82e5e12.txt", mailjet_probe),
 ]
+
+def mailjet_probe(_):
+    # пустой ответ text/plain
+    return HttpResponse("", content_type="text/plain")
