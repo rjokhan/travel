367b4d09113ba09a59dd54f49ccec65f108f321a
rjokhan <jaidonswift@gmail.com>
2025-08-23 17:26:04 +0500

    update

--- travelweb/travelapp/views.py
+++ travelweb/travelapp/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Trip
 from django.views.decorators.csrf import ensure_csrf_cookie
+from .models import Trip
 
+@ensure_csrf_cookie
 def index(request):
-    trips = Trip.objects.filter(is_published=True).select_related("country").order_by("-date_start", "title_full")
+    trips = (
+        Trip.objects
+        .filter(is_published=True)
+        .select_related("country")
+        .order_by("-date_start", "title_full")
+    )
     return render(request, "index.html", {"trips": trips})
 
 def trip_detail(request, slug):
     trip = get_object_or_404(Trip, slug=slug, is_published=True)
     return render(request, "trip.html", {"trip": trip})
-
-@ensure_csrf_cookie
-def index(request):
-    trips = Trip.objects.filter(is_published=True).select_related("country").order_by("-date_start", "title_full")
-    return render(request, "index.html", {"trips": trips})
